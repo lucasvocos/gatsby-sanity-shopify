2e107af912c8c20bc7fbfdf2afda380697c1a530
Lucas Vocos <lucasvocos@gmail.com>
2020-07-20 16:51:31 -0400

    update header & footer imports

--- web/src/components/Layout/Layout.js
+++ web/src/components/Layout/Layout.js
@@ -8,8 +8,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import Header from "../Header/Header"
-import Footer from "../Footer/Footer"
+import Header from "../Header"
+import Footer from "../Footer"
 import "./reset.css"
 import "./Layout.scss"
 
